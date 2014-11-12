cef04c04d2cd430e5d90cd2bf377b7d0ccfc4dd8
Daniel Schlaug <daniel.schlaug@gmail.com>
2014-11-12 16:07:35 +0100

    Added test of getNodeInDirection and fixed bugs

--- tests/kth/game/othello/simple/SimpleBoardTest.java
+++ tests/kth/game/othello/simple/SimpleBoardTest.java
@@ -1,24 +1,26 @@
 package kth.game.othello.simple;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import kth.game.othello.board.Node;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import static org.junit.Assert.*;
+
 public class SimpleBoardTest {
+
+    private final String dummyID = "dummyID";
+    private final SimpleBoard dummy8x8Board = generateBoardWithSide(8);
 
     private List<Node> generateNDummyNodes(int numberOfNodes) {
 
         // Mock dummy node
         Node dummyNode = Mockito.mock(Node.class);
-        Mockito.when(dummyNode.getId()).thenReturn("dummy");
+        Mockito.when(dummyNode.getId()).thenReturn(dummyID);
 
         // Construct a list of dummy nodes
         List<Node> dummyNodes = new ArrayList<Node>();
@@ -57,33 +59,35 @@ public class SimpleBoardTest {
 
         for (Node node : board.getNodes()) {
             if (node.getId() == specificNode.getId()) {
-                fail("SimpleBoard was not immutable.");
+                fail("SimpleBoard should be immutable but could be mutated through its returned list of nodes.");
             }
         }
 
 	}
 
-    @Test
-    public void testGetNodeOutOfBoundsShouldReturnNull() throws Exception {
 
-        final int boardSide = 8;
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetNodeOutOfRightBoundsShouldReturnNull() throws Exception {
+        assertNull(dummy8x8Board.getNodeAtCoordinates(8,0));
+    }
 
-        SimpleBoard board = generateBoardWithSide(boardSide);
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetNodeOutOfBottomBoundsShouldReturnNull() throws Exception {
+        assertNull(dummy8x8Board.getNodeAtCoordinates(0,-1));
+    }
 
-        Node nodeOutOfRightRange = board.getNodeAtCoordinates(8,0);
-        Node nodeOutOfLeftRange = board.getNodeAtCoordinates(-1,0);
-        Node nodeOutOfTopRange = board.getNodeAtCoordinates(0,8);
-        Node nodeOutOfBottomRange = board.getNodeAtCoordinates(0,-1);
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetNodeOutOfTopBoundsShouldReturnNull() throws Exception {
+        assertNull(dummy8x8Board.getNodeAtCoordinates(0,8));
+    }
 
-        assertNull(nodeOutOfBottomRange);
-        assertNull(nodeOutOfTopRange);
-        assertNull(nodeOutOfRightRange);
-        assertNull(nodeOutOfLeftRange);
-
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetNodeOutOfLeftBoundsShouldReturnNull() throws Exception {
+        assertNull(dummy8x8Board.getNodeAtCoordinates(-1,0));
     }
 
     @Test
-	public void testGetNodeAtCoordinatesShouldBeCartesian() throws Exception {
+	public void testCoordinatesShouldBeCartesian() throws Exception {
 
         final int boardSide = 8;
         final int boardSize = boardSide * boardSide;
@@ -112,4 +116,59 @@ public class SimpleBoardTest {
         assertEquals(board.getNodeAtCoordinates(7, 0).getId(), lowerRightNode.getId());
 
 	}
+
+    @Test
+    public void testGetNodeByID() throws Exception {
+
+        SimpleBoard board = generateBoardWithSide(1);
+
+        assertNotNull(board.getNodeById(dummyID));
+
+        assertNull(board.getNodeById("some random ID that shouldn't exist in board 37892783"));
+
+    }
+
+    @Test
+    public void testGetNextNodeInDirection() throws Exception {
+
+        final int boardSide = 3;
+        final int boardSize = boardSide * boardSide;
+
+        // Construct a board with nodeIDs like these:
+        // NW N NE
+        //  W D E       Where NW, N, SE etc. are the 8 directions and D is the starting node.
+        // SW S SE
+        List<Node> nodes = new ArrayList<Node>();
+        String middleID = "middle";
+        String[] directions = new String[] {
+            SimpleBoard.Direction.NORTHWEST.name(),
+            SimpleBoard.Direction.NORTH.name(),
+            SimpleBoard.Direction.NORTHEAST.name(),
+            SimpleBoard.Direction.WEST.name(),
+            middleID,
+            SimpleBoard.Direction.EAST.name(),
+            SimpleBoard.Direction.SOUTHWEST.name(),
+            SimpleBoard.Direction.SOUTH.name(),
+            SimpleBoard.Direction.SOUTHEAST.name()
+        };
+        for (String nodeID : directions) {
+            Node node = Mockito.mock(Node.class);
+            Mockito.when(node.getId()).thenReturn(nodeID);
+            Mockito.when(node.getXCoordinate()).thenReturn(1);
+            Mockito.when(node.getYCoordinate()).thenReturn(1);
+            nodes.add(node);
+        }
+
+        // Create board
+        SimpleBoard board = new SimpleBoard(nodes);
+
+        Node middleNode = board.getNodeAtCoordinates(1,1);
+        assertEquals(middleID, middleNode.getId());
+
+        for (SimpleBoard.Direction direction : SimpleBoard.Direction.values()) {
+            Node nodeInDirection = board.getNextNodeInDirection(middleNode, direction);
+            assertEquals(direction.name(), nodeInDirection.getId());
+        }
+
+    }
 }
