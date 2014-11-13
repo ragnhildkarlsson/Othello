2809c053fbd060581f7042c3a5b64496556b6741
ragnhild karlsson <ragnhild.karlsson@gmail.com>
2014-11-13 18:21:24 +0100

    added that testmthods ensure use of players Id

--- tests/kth/game/othello/simple/ComputerPlayerTest.java
+++ tests/kth/game/othello/simple/ComputerPlayerTest.java
@@ -17,41 +17,50 @@ public class ComputerPlayerTest {
 	 */
 	@Test
 	public void testWhenValidMoveExistsShouldGetMoveReturnValidMove() {
+		String testPlayerId = "id";
+		ComputerPlayer testPlayer = new ComputerPlayer(testPlayerId, null);
 
 		ArrayList<Node> nodesOnBoard = new ArrayList<>();
 		Node unvalidMove = Mockito.mock(Node.class);
 		nodesOnBoard.add(unvalidMove);
 		Node validMove = Mockito.mock(Node.class);
 		nodesOnBoard.add(validMove);
+
 		SimpleBoard mockBoard = Mockito.mock(SimpleBoard.class);
 		Mockito.when(mockBoard.getNodes()).thenReturn(nodesOnBoard);
 		SimpleRules mockRules = Mockito.mock(SimpleRules.class);
-		Mockito.when(mockRules.validMove(mockBoard, validMove, null)).thenReturn(true);
-		Mockito.when(mockRules.validMove(mockBoard, unvalidMove, null)).thenReturn(false);
-		ComputerPlayer testPlayer = new ComputerPlayer(null, null);
+
+		Mockito.when(mockRules.validMove(mockBoard, validMove, testPlayerId)).thenReturn(true);
+		Mockito.when(mockRules.validMove(mockBoard, unvalidMove, testPlayerId)).thenReturn(false);
+
 		assertEquals("The valid move should be returned", validMove, testPlayer.getMove(mockRules, mockBoard));
 		Mockito.verify(mockBoard).getNodes();
-		Mockito.verify(mockRules).validMove(mockBoard, validMove, null);
-		Mockito.verify(mockRules).validMove(mockBoard, unvalidMove, null);
+		Mockito.verify(mockRules).validMove(mockBoard, validMove, testPlayerId);
+		Mockito.verify(mockRules).validMove(mockBoard, unvalidMove, testPlayerId);
 	}
 
 	/**
 	 * Test that given a board where no valid moves exist, then should getMove
 	 * return null
 	 */
+	@Test
 	public void testWhenNoValidMoveExistsShouldGetMoveReturnNull() {
+		String testPlayerId = "id";
+		ComputerPlayer testPlayer = new ComputerPlayer(testPlayerId, null);
+
 		ArrayList<Node> nodesOnBoard = new ArrayList<>();
 		Node unvalidMove = Mockito.mock(Node.class);
 		nodesOnBoard.add(unvalidMove);
+
 		SimpleBoard mockBoard = Mockito.mock(SimpleBoard.class);
 		Mockito.when(mockBoard.getNodes()).thenReturn(nodesOnBoard);
 		SimpleRules mockRules = Mockito.mock(SimpleRules.class);
 		Mockito.when(mockRules.validMove(mockBoard, unvalidMove, null)).thenReturn(false);
-		ComputerPlayer testPlayer = new ComputerPlayer(null, null);
-		assertEquals("The that null is returned when no valid move exists", null,
+
+		assertEquals("Check that null is returned when no valid move exists", null,
 				testPlayer.getMove(mockRules, mockBoard));
 		Mockito.verify(mockBoard).getNodes();
-		Mockito.verify(mockRules).validMove(mockBoard, unvalidMove, null);
+		Mockito.verify(mockRules).validMove(mockBoard, unvalidMove, testPlayerId);
 
 	}
 }
