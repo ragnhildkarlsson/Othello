95f712c3791a087f56f4e8223a8ef5566e3e0d68
ragnhild karlsson <ragnhild.karlsson@gmail.com>
2014-11-21 12:57:18 +0100

    added missin coord-class

---
+++ src/kth/game/othello/simple/board/ImmutableXYCoordinates.java
@@ -0,0 +1,43 @@
+package kth.game.othello.simple.board;
+
+public class ImmutableXYCoordinates {
+	private final int xCoordinate;
+	private final int yCoordinate;
+
+	public ImmutableXYCoordinates(int xCoordinate, int yCoordinate) {
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = yCoordinate;
+	}
+
+	public int getXCoordinate() {
+		return xCoordinate;
+	}
+
+	public int getYCoordinate() {
+		return xCoordinate;
+	}
+
+	@Override
+	public boolean equals(Object otherXYCoord) {
+		// TODO Implement
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO implement
+		// Maybe helpful old implementation of id
+		int idCode = xCoordinate << 16;
+		idCode = idCode | yCoordinate;
+		// this.nodeId = Integer.toString(idCode);
+
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		return "{x=" + xCoordinate + ", y=" + yCoordinate + "}";
+
+	}
+
+}
