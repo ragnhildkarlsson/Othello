69ad67def9ca2447fc2114ef0913342d8737cf13
zhori <aldur92@hotmail.com>
2014-11-24 19:31:54 +0100

    Fixed GameModel and added turnkeeper, movemaker removed

--- src/kth/game/othello/simple/model/GameModel.java
+++ src/kth/game/othello/simple/model/GameModel.java
@@ -4,42 +4,59 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * This class is responsible for ... TODO
+ * This class represents a model of an Othello game. It is responsible for all
+ * interaction with the model.
  * 
  */
 public class GameModel {
 
-	private MoveMaker moveMaker;
+	private TurnKeeper turnKeeper;
+	private Rules rules;
+	private ImmutableBoard board;
 
 	/**
-	 * TODO
+	 * Create a new GameModel.
+	 * 
+	 * @param rules
+	 *            the rules of the game.
+	 * 
+	 * @param turnKeeper
+	 *            the turn keeper of the game.
+	 * 
+	 * @param ImmutableBoard
+	 *            the starting of the game.
 	 */
-	protected GameModel(MoveMaker moveMaker) {
-		this.moveMaker = moveMaker;
+	protected GameModel(Rules rules, TurnKeeper turnKeeper, ImmutableBoard startingBoard) {
+		this.rules = rules;
+		this.board = startingBoard;
+		this.turnKeeper = turnKeeper;
 	}
 
 	/**
-	 * Returns a set with nodes that will be swapped to belong to the player
-	 * with the given id if he/she play at a node with the given coordinates;
+	 * Returns a set with coordinates of the nodes that will be swapped to
+	 * belong to the player with the given id if he/she play at a node with the
+	 * given coordinates;
 	 * 
 	 * @param playerId
 	 *            the id of the player making the move
 	 * @param nodeCoordinates
 	 *            the coordinates of the node where the move is made
-	 * @return Returns a set with nodes that will be swapped if the player with
-	 *         the given id play at a node with the given coordinates.
+	 * @return Returns a set with coordinates of the nodes that will be swapped
+	 *         if the player with the given id play at a node with the given
+	 *         coordinates.
 	 */
 	public Set<Coordinates> getNodesToSwap(String playerId, Coordinates nodeCoordinates) {
-		return getSetOfCoordinatesFromNodes(moveMaker.getNodesToSwap(nodeCoordinates, playerId));
+		return getSetOfCoordinatesFromNodes(rules.getNodesToSwap(board, board.getNodeAtCoordinates(nodeCoordinates),
+				playerId));
 	}
 
 	/**
-	 * Get the id of the player in turn or null if no player can move
+	 * Get the id of the player in turn or null if no player can move.
 	 * 
 	 * @return the id of the player in turn
 	 */
 	public String getPlayerInTurn() {
-		return moveMaker.getPlayerInTurn();
+		return turnKeeper.getPlayerInTurn(rules, board);
 	}
 
 	/**
@@ -47,32 +64,32 @@ public class GameModel {
 	 * 
 	 * @param playerId
 	 *            the id of the player
-	 * @return true if the player with the given id has a valid move
+	 * @return true if the player with the given id has a valid move.
 	 */
 	public boolean hasValidMove(String playerId) {
-		return moveMaker.hasValidMove(playerId);
+		return rules.hasValidMove(board, playerId);
 	}
 
 	/**
-	 * Determines if the game is active or over
+	 * Determines if the game is active or over.
 	 * 
-	 * @return false if the game is over
+	 * @return false if the game is over.
 	 */
 	public boolean isActive() {
-		return moveMaker.isActive();
+		return !rules.isGameOver(board);
 	}
 
 	/**
 	 * Determines if a player is allowed to make a move at the given node.
 	 * 
 	 * @param playerId
-	 *            the id of the player making the move
+	 *            the id of the player making the move.
 	 * @param nodeCoordinates
-	 *            the coordinates of the node where the player wants to play
-	 * @return true if the move is valid
+	 *            the coordinates of the node where the player wants to play.
+	 * @return true if the move is valid.
 	 */
 	public boolean isMoveValid(String playerId, Coordinates nodeCoordinates) {
-		return moveMaker.isMoveValid(playerId, nodeCoordinates);
+		return rules.validMove(board, board.getNodeAtCoordinates(nodeCoordinates), playerId);
 	}
 
 	/**
@@ -81,15 +98,33 @@ public class GameModel {
 	 * 
 	 * @param playerId
 	 *            the id of the player that makes the move
-	 * @param nodeCoordinates
+	 * @param nodeCoordinate
 	 *            the coordinates of the node where the player wants to move
 	 * @return the coordinates of the nodes that where swapped, including the
 	 *         coordinates of the node where the player made the move;
 	 * @throws IllegalArgumentException
 	 *             if the move is not valid, or if the player is not in turn
 	 */
-	public Set<Coordinates> move(String playerId, Coordinates nodeCoordinates) throws IllegalArgumentException {
-		return getSetOfCoordinatesFromNodes(moveMaker.makeMove(playerId, nodeCoordinates));
+	public Set<Coordinates> move(String playerId, Coordinates nodeCoordinate) throws IllegalArgumentException {
+		// Check that this player is in turn
+		ImmutableNode nodeToPlay = board.getNodeAtCoordinates(nodeCoordinate);
+		if (!turnKeeper.getPlayerInTurn(rules, board).equals(playerId)) {
+			throw new IllegalArgumentException("Trying to make a move with a player not in turn");
+		}
+		// Check that the move is valid
+		if (!rules.validMove(board, nodeToPlay, playerId)) {
+			throw new IllegalArgumentException("Trying to make unvalid move");
+		}
+		// Get the nodes that will be swapped
+		Set<ImmutableNode> nodesToSwap = rules.getNodesToSwap(board, nodeToPlay, playerId);
+
+		// perform the move
+		board = board.swapNodes(nodesToSwap, playerId);
+
+		// go to the next turn
+		turnKeeper.nextTurn();
+
+		return getSetOfCoordinatesFromNodes(nodesToSwap);
 	}
 
 	private Set<Coordinates> getSetOfCoordinatesFromNodes(Set<ImmutableNode> nodes) {
---
+++ src/kth/game/othello/simple/model/TurnKeeper.java
@@ -0,0 +1,57 @@
+package kth.game.othello.simple.model;
+
+import java.util.List;
+
+/**
+ * This class is responsible for keeping the current player in turn to play the
+ * game.
+ * 
+ * @author mikael
+ * 
+ */
+public class TurnKeeper {
+
+	private List<String> players;
+	private int playerInTurn;
+
+	/**
+	 * Create a new turnKeeper.
+	 * 
+	 * @param playerIds
+	 * @param startingPlayerId
+	 */
+	public TurnKeeper(List<String> playerIds, String startingPlayerId) {
+		this.players = playerIds;
+		this.playerInTurn = players.indexOf(startingPlayerId);
+
+	}
+
+	/**
+	 * Get the id of the player in turn or null if no player can move
+	 * 
+	 * @return the id of the player in turn or null if no player can move;
+	 */
+	public String getPlayerInTurn(Rules rules, ImmutableBoard board) {
+		for (int i = 0; i < players.size(); i++) {
+			String possiblePlayerInTurn = players.get(i);
+			if (rules.hasValidMove(board, possiblePlayerInTurn)) {
+				return possiblePlayerInTurn;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Change the current player in turn to the next player.
+	 */
+	public void nextTurn() {
+		playerInTurn = playerInTurn + 1 % players.size();
+	}
+
+	/**
+	 * @return The number of players.
+	 */
+	public int getNumberOfPlayers() {
+		return this.players.size();
+	}
+}
