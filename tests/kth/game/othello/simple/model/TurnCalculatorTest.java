40b54fb2971e87bb95e94ed301f3be61b497219c
zhori <aldur92@hotmail.com>
2014-11-25 22:48:33 +0100

    Fixed error in turncalculator and made unit test catch the error

--- tests/kth/game/othello/simple/model/TurnCalculatorTest.java
+++ tests/kth/game/othello/simple/model/TurnCalculatorTest.java
@@ -26,13 +26,13 @@ public class TurnCalculatorTest {
 
 		Rules rules = Mockito.mock(Rules.class);
 		Mockito.when(rules.hasValidMove(null, player1Id)).thenReturn(true);
-		Mockito.when(rules.hasValidMove(null, player2Id)).thenReturn(false);
+		Mockito.when(rules.hasValidMove(null, player2Id)).thenReturn(true);
 		Mockito.when(rules.hasValidMove(null, player3Id)).thenReturn(false);
 
 		TurnCalculator turnCalculator = new TurnCalculator(playerIds);
 
 		String playerInTurn = turnCalculator.getPlayerInTurn(player2Id, null, rules);
-
+		// Should return player1 since player2 was the previous player.
 		assertEquals(player1Id, playerInTurn);
 
 	}
