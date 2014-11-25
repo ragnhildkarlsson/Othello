bd478e994e49f771e4cc1ba5875aecbfa67187bd
zhori <aldur92@hotmail.com>
2014-11-25 20:21:49 +0100

    Created GameModel Integration test

---
+++ tests/kth/game/othello/simple/model/GameModelITest.java
@@ -0,0 +1,12 @@
+package kth.game.othello.simple.model;
+
+/**
+ * Integration test of the main class GameModel, including all classes in the
+ * model package.
+ * 
+ * @author mikael
+ *
+ */
+public class GameModelITest {
+
+}
--- tests/kth/game/othello/simple/model/ImmutableBoardFactoryTest.java
+++
@@ -1,9 +0,0 @@
-package kth.game.othello.simple.model;
-
-
-public class ImmutableBoardFactoryTest {
-	
-	// TODO
-	// Test new board generation
-
-}
