211de54657d3556f037082f2277c2ee7a4497559
zhori <aldur92@hotmail.com>
2014-12-08 19:21:05 +0100

    Fixed so SimpleStrategy works with new Rules

--- src/kth/game/othello/player/movestrategy/SimpleStrategy.java
+++ src/kth/game/othello/player/movestrategy/SimpleStrategy.java
@@ -1,13 +1,13 @@
 package kth.game.othello.player.movestrategy;
 
-import kth.game.othello.Othello;
 import kth.game.othello.board.Board;
 import kth.game.othello.board.Node;
 import kth.game.othello.rules.Rules;
 
 /**
- * A simple strategy to perform a move for a computer player, taking a random
- * valid move.
+ * A simple strategy to perform a move for a computer player, taking the first
+ * available move based on the order of the nodes it is given.
+ * 
  */
 public class SimpleStrategy implements MoveStrategy {
 
@@ -20,9 +20,8 @@ public class SimpleStrategy implements MoveStrategy {
 	}
 
 	/**
-	 * Determines which node the given player will move at, this will be a
-	 * random valid move or null if no valid move exists.
-	 * 
+	 * Determines which node the given player will move at, taking the first
+	 * available move based on the order of the nodes it is given.
 	 * 
 	 * @param playerId
 	 *            the id of the player that will make a move
@@ -31,24 +30,18 @@ public class SimpleStrategy implements MoveStrategy {
 	 * @return the node where the player wants to move. If the player is not
 	 *         able to move then null is returned.
 	 */
-	// TODO REMOVE
-	public Node move(String playerId, Othello othello) {
-		if (!othello.hasValidMove(playerId)) {
+	@Override
+	public Node move(String playerId, Rules rules, Board board) {
+		if (!rules.hasValidMove(playerId)) {
 			return null;
 		}
-		java.util.List<Node> nodesOnBoard = othello.getBoard().getNodes();
+		java.util.List<Node> nodesOnBoard = board.getNodes();
 		for (Node node : nodesOnBoard) {
-			if (othello.isMoveValid(playerId, node.getId())) {
+			if (rules.isMoveValid(playerId, node.getId())) {
 				return node;
 			}
 
 		}
-		return null;
-	}
-
-	@Override
-	public Node move(String playerId, Rules rules, Board board) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 }
