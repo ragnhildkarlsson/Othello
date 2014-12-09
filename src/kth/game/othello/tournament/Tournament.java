9fdf4aa893bf710320fb6ce77c7689432f9fae68
zhori <aldur92@hotmail.com>
2014-12-09 14:00:23 +0100

    Skeletons

---
+++ src/kth/game/othello/tournament/Tournament.java
@@ -0,0 +1,13 @@
+package kth.game.othello.tournament;
+
+public class Tournament {
+
+	public void startViewableTournament() {
+
+	}
+
+	public void startTournament() {
+
+	}
+
+}
---
+++ src/kth/game/othello/tournament/TournamentFactory.java
@@ -0,0 +1,13 @@
+package kth.game.othello.tournament;
+
+import java.util.List;
+
+import kth.game.othello.player.Player;
+
+public class TournamentFactory {
+
+	public Tournament generateComputerTournament(List<Player> computerPlayers) {
+		return null;
+	}
+
+}
